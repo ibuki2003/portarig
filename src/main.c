cd020dcbed29c0061114a45965c91d5a002adf8c
ibuki2003 <main@fuwa.dev>
2023-02-08 21:32:06 +0900

    feat: setup usb

--- src/main.c
+++ src/main.c
@@ -4,20 +4,53 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <stdint.h>
+#include "hardware/structs/uart.h"
 #include "pico/stdlib.h"
 
+#include "bsp/board.h"
+#include "tusb_config.h"
+
+const uint LED_PIN = 25;
+
+static void cdc_task(void);
+
+bool led_state = false;
+static bool alarm_irq(repeating_timer_t *alarm) {
+    hw_clear_bits(&timer_hw->intr, 0b1);
+    led_state = !led_state;
+    gpio_put(LED_PIN, led_state);
+    return true;
+}
+
 int main() {
-#ifndef PICO_DEFAULT_LED_PIN
-#warning blink example requires a board with a regular LED
-#else
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    /* stdio_init_all(); */
+    board_init();
+    tusb_init();
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (true) {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
+
+    repeating_timer_t timer;
+    add_repeating_timer_ms(-500, alarm_irq, NULL, &timer);
+
+    while (1) {
+        tud_task(); // tinyusb device task
+        cdc_task();
     }
-#endif
+
+}
+
+static void cdc_task(void) {
+    uint8_t itf;
+    for (itf = 0; itf < CFG_TUD_CDC; itf++) {
+        if (tud_cdc_n_available(itf)) {
+            uint8_t buf[64];
+            uint32_t count = tud_cdc_n_read(itf, buf, sizeof(buf));
+
+            // echo back
+            tud_cdc_n_write(itf, buf, count);
+            tud_cdc_n_write_flush(itf);
+        }
+    }
+
 }
---
+++ src/usb_desciptors.c
@@ -0,0 +1,100 @@
+#include "device/usbd.h"
+#include "tusb.h"
+#include "tusb_config.h"
+#include "tusb_types.h"
+
+
+tusb_desc_device_t const device_desc = {
+    .bLength = sizeof(tusb_desc_device_t),
+    .bDescriptorType = TUSB_DESC_DEVICE,
+    .bcdUSB = 0x0110,
+    .bDeviceClass = 0x00,
+    .bDeviceSubClass = 0x00,
+    .bDeviceProtocol = 0x00,
+    .bMaxPacketSize0 = CFG_TUD_ENDPOINT0_SIZE,
+    .idVendor = 0x1234,
+    .idProduct = 0xabcd,
+    .bcdDevice = 0x0000,
+    .iManufacturer = 0x00,
+    .iProduct = 0x01,
+    .iSerialNumber = 0x00,
+    .bNumConfigurations = 0x01,
+};
+uint8_t const *tud_descriptor_device_cb(void) {
+    return (uint8_t const *) &device_desc;
+}
+
+enum
+{
+  ITF_NUM_CDC_0 = 0,
+  ITF_NUM_CDC_0_DATA,
+  ITF_NUM_TOTAL
+};
+
+#define EPNUM_CDC_0_NOTIF   0x81
+#define EPNUM_CDC_0_OUT     0x02
+#define EPNUM_CDC_0_IN      0x82
+
+#define CONFIG_TOTAL_LEN    ( \
+    + TUD_CONFIG_DESC_LEN \
+    + TUD_CDC_DESC_LEN * CFG_TUD_CDC \
+)
+
+uint8_t const conf_desc[] = {
+    // Config number, interface count, string index, total length, attribute, power in mA
+    TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, 0x00, 100),
+    TUD_CDC_DESCRIPTOR(ITF_NUM_CDC_0, 4, EPNUM_CDC_0_NOTIF, 8, EPNUM_CDC_0_OUT, EPNUM_CDC_0_IN, 64),
+};
+
+uint8_t const *tud_descriptor_configuration_cb(uint8_t index) {
+    return conf_desc;
+}
+
+char const* string_desc_arr [] =
+{
+  (const char[]) { 0x09, 0x04 }, // 0: is supported language is English (0x0409)
+  "TinyUSB",                     // 1: Manufacturer
+  "TinyUSB Device",              // 2: Product
+  "123456",                      // 3: Serials, should use chip ID
+  "TinyUSB CDC",                 // 4: CDC Interface
+};
+
+static uint16_t _desc_str[32];
+
+// Invoked when received GET STRING DESCRIPTOR request
+// Application return pointer to descriptor, whose contents must exist long enough for transfer to complete
+uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid)
+{
+  (void) langid;
+
+  uint8_t chr_count;
+
+  if ( index == 0)
+  {
+    memcpy(&_desc_str[1], string_desc_arr[0], 2);
+    chr_count = 1;
+  }else
+  {
+    // Note: the 0xEE index string is a Microsoft OS 1.0 Descriptors.
+    // https://docs.microsoft.com/en-us/windows-hardware/drivers/usbcon/microsoft-defined-usb-descriptors
+
+    if ( !(index < sizeof(string_desc_arr)/sizeof(string_desc_arr[0])) ) return NULL;
+
+    const char* str = string_desc_arr[index];
+
+    // Cap at max char
+    chr_count = (uint8_t) strlen(str);
+    if ( chr_count > 31 ) chr_count = 31;
+
+    // Convert ASCII string into UTF-16
+    for(uint8_t i=0; i<chr_count; i++)
+    {
+      _desc_str[1+i] = str[i];
+    }
+  }
+
+  // first byte is length (including header), second byte is string type
+  _desc_str[0] = (uint16_t) ((TUSB_DESC_STRING << 8 ) | (2*chr_count + 2));
+
+  return _desc_str;
+}
